17e9997a6d5fed0ef345599f164ddbbf9e06b7db
Adam Paul <notadampaul@protonmail.com>
2025-02-28 16:24:28 -0600

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'goat-in-the-shell-demo.up.railway.app',
+      '.railway.app', // This will allow all railway.app subdomains
+    ],
+  },
 })
